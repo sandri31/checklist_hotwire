bb9a48b055b9960bf61c5ae1752462163b86fed5
Florent S <sandri822@hotmail.fr>
2023-11-14 20:40:45 +0100

    ✨ Add dependent destroy to Recipe model and optional true to RecipeStep model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_steps
+  has_many :recipe_steps, dependent: :destroy
   accepts_nested_attributes_for :recipe_steps,
                                 reject_if: :all_blank, allow_destroy: true
 end
