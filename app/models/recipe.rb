d599255f9bf612cea4cfc6a8a654909f94d4261e
Florent S <sandri822@hotmail.fr>
2023-11-14 16:57:46 +0100

    🚧 Add recipe steps to recipe form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_steps
+  accepts_nested_attributes_for :recipe_steps,
+                                reject_if: :all_blank, allow_destroy: true
 end
